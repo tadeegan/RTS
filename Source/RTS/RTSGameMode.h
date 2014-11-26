c6740f7da52dbf93b0821545e7439df3d0597dd8
vilepig <mddeegan@gmail.com>
2014-11-26 18:56:37 -0500

    First Commit

---
+++ Source/RTS/RTSGameMode.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "RTSGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RTS_API ARTSGameMode : public AGameMode
+{
+	GENERATED_UCLASS_BODY()
+
+	
+	
+};
